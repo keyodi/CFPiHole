23fb1cbe41a01c31a10686449a4469228e25c910
keyodi <zeusomighty@hotmail.com>
2024-06-15 10:45:26 -0700

    Add check to make sure conig.ini exists

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import cloudflare
 import configparser
 import pandas as pd
 import os
+import time
 
 class App:
     def __init__(self):        
@@ -14,103 +15,111 @@ class App:
       # self.whitelist = self.loadWhitelist()
         self.tldlist = self.loadTldlist()
 
-    #def loadWhitelist(self):
+    # def loadWhitelist(self):
     #    return open("whitelist.txt", "r").read().split("\n")
 
     def loadTldlist(self):
         return open("tldlist.txt", "r").read().split("\n")
 
     def run(self):
+
+        #check tmp dir exists
+        try:
+            os.makedirs("./tmp", exist_ok=True)
+        except OSError as error:
+            print("\033[0;31;40m ERROR: unable to create tmp folder\033[0;0m")
         
-        config = configparser.ConfigParser()
-        config.read('config.ini')
+        file_path = 'config.ini'
+        if os.path.exists(file_path):
+         
+            config = configparser.ConfigParser()
+            config.read('config.ini')
 
-        #check tmp dir
-        os.makedirs("./tmp", exist_ok=True)
+            all_domains = []
+            for list in config["Lists"]:
 
-        all_domains = []
-        for list in config["Lists"]:
-
-            print ("Setting list " +  list)
+                print ("Setting list " +  list)
             
-            name_prefix = f"[AdBlock-{list}]"
+                name_prefix = f"[AdBlock-{list}]"
 
-            self.download_file(config["Lists"][list], list)
-            domains = self.convert_to_domain_list(list)
-            all_domains = all_domains + domains
+                self.download_file(config["Lists"][list], list)
+                domains = self.convert_to_domain_list(list)
+                all_domains = all_domains + domains
 
-        unique_domains = pd.unique(all_domains)
+            unique_domains = pd.unique(all_domains)
 
-        # check if the list is already in Cloudflare
-        cf_lists = cloudflare.get_lists(self.name_prefix)
+            # check if the list is already in Cloudflare
+            cf_lists = cloudflare.get_lists(self.name_prefix)
 
-        self.logger.info(f"Number of lists in Cloudflare: {len(cf_lists)}")
+            self.logger.info(f"Number of lists in Cloudflare: {len(cf_lists)}")
 
-        # compare the lists size
-        if len(unique_domains) == sum([l["count"] for l in cf_lists]):
-            self.logger.warning("Lists are the same size, skipping")
+            # compare the lists size
+            if len(unique_domains) == sum([l["count"] for l in cf_lists]):
+                self.logger.warning("Lists are the same size, skipping")
+
+            else:
+
+                #delete the policy
+
+                cf_policies = cloudflare.get_firewall_policies(self.name_prefix)            
+                if len(cf_policies)>0:
+                    cloudflare.delete_firewall_policy(cf_policies[0]["id"])
+
+                # delete the lists
+                for l in cf_lists:
+                    self.logger.info(f"Deleting list {l['name']}")
+                    # Sleep to prevent rate limit
+                    time.sleep(1)
+                    cloudflare.delete_list(l["id"])
+
+                cf_lists = []
+
+                # chunk the domains into lists of 1000 and create them
+                for chunk in self.chunk_list(unique_domains, 1000):
+                    list_name = f"{self.name_prefix} {len(cf_lists) + 1}"
+
+                    self.logger.info(f"Creating list {list_name}")
+
+                    _list = cloudflare.create_list(list_name, chunk)
+
+                    # Sleep to prevent rate limit
+                    time.sleep(1)
+
+                    cf_lists.append(_list)
+
+            # get the gateway policies
+            cf_policies = cloudflare.get_firewall_policies(self.name_prefix)
+
+            self.logger.info(f"Number of policies in Cloudflare: {len(cf_policies)}")
+
+            # setup the gateway policy
+            if len(cf_policies) == 0:
+                self.logger.info("Creating firewall policy")
+
+                cf_policies = cloudflare.create_gateway_policy(f"{self.name_prefix} Block Ads", [l["id"] for l in cf_lists])
+
+            elif len(cf_policies) != 1:
+                self.logger.error("More than one firewall policy found")
+
+                raise Exception("More than one firewall policy found")
+
+            else:
+                self.logger.info("Updating firewall policy")
+
+                cloudflare.update_gateway_policy(f"{self.name_prefix} Block Ads", cf_policies[0]["id"], [l["id"] for l in cf_lists])
+
+            self.logger.info("Done")
 
         else:
-
-            #delete the policy
-
-            cf_policies = cloudflare.get_firewall_policies(self.name_prefix)            
-            if len(cf_policies)>0:
-                cloudflare.delete_firewall_policy(cf_policies[0]["id"])
-
-            # delete the lists
-            for l in cf_lists:
-                self.logger.info(f"Deleting list {l['name']}")
-
-                # Sleep to prevent rate limit
-                time.sleep(1)
-
-                cloudflare.delete_list(l["id"])
-
-            cf_lists = []
-
-            # chunk the domains into lists of 1000 and create them
-            for chunk in self.chunk_list(unique_domains, 1000):
-                list_name = f"{self.name_prefix} {len(cf_lists) + 1}"
-
-                self.logger.info(f"Creating list {list_name}")
-
-                _list = cloudflare.create_list(list_name, chunk)
-
-                # Sleep to prevent rate limit
-                time.sleep(1)
-
-                cf_lists.append(_list)
-
-        # get the gateway policies
-        cf_policies = cloudflare.get_firewall_policies(self.name_prefix)
-
-        self.logger.info(f"Number of policies in Cloudflare: {len(cf_policies)}")
-
-        # setup the gateway policy
-        if len(cf_policies) == 0:
-            self.logger.info("Creating firewall policy")
-
-            cf_policies = cloudflare.create_gateway_policy(f"{self.name_prefix} Block Ads", [l["id"] for l in cf_lists])
-
-        elif len(cf_policies) != 1:
-            self.logger.error("More than one firewall policy found")
-
-            raise Exception("More than one firewall policy found")
-
-        else:
-            self.logger.info("Updating firewall policy")
-
-            cloudflare.update_gateway_policy(f"{self.name_prefix} Block Ads", cf_policies[0]["id"], [l["id"] for l in cf_lists])
-
-        self.logger.info("Done")
+            print("\033[0;31;40m ERROR: config.ini does not exist\033[0;0m")
+            
 
     def is_valid_hostname(self, hostname):
         import re
         if len(hostname) > 255:
             return False
         hostname = hostname.rstrip(".")
-        allowed = re.compile('^[a-z0-9]([a-z0-9\-\_]{0,61}[a-z0-9])?$',re.IGNORECASE)
+        allowed = re.compile(r'^[a-z0-9]([a-z0-9\-\_]{0,61}[a-z0-9])?$',re.IGNORECASE)
         labels = hostname.split(".")
         
         # the TLD must not be all-numeric
@@ -135,19 +144,19 @@ class App:
         with open("tmp/"+file_name, "r") as f:
             data = f.read()
 
-        # TODO: Temp fix to account for hosts or domains that contain each iteration
+        # TODO: temp fix to account for hosts or domains contained in each iteration
         # check if the file is a hosts file or a list of domain
         is_hosts_file = False
         for ip in ["localhost ", "127.0.0.1 ", "::1 ", "0.0.0.0 "]:
-            if ip in data:
-                is_hosts_file = True
-                break
+           if ip in data:
+               is_hosts_file = True
+               break
 
         domains = []
- 
+        
         # check for TLDs
         tld = self.tldlist
-        
+
         for line in data.splitlines():
 
             
@@ -155,7 +164,7 @@ class App:
             if line.startswith("#") or line.startswith(";") or line == "\n" or line == "":
                 continue
 
-            # skip tld if in list
+            # skip tld is in List
             if not line.endswith(tuple(tld)):
                continue
 
@@ -185,6 +194,7 @@ class App:
 
     def chunk_list(self, _list: List[str], n: int):
         for i in range(0, len(_list), n):
+
             yield _list[i : i + n]
 
 
@@ -193,6 +203,3 @@ if __name__ == "__main__":
 
     app = App()
     app.run()
-
-
-    
