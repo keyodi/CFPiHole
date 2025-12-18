a8845ddbc2f57d5701dca9e2a7a8e89801a84451
Antony Kolitsos <zeusomighty@hotmail.com>
2025-12-17 17:33:45 -0800

    logger_config.py: cleanup source for readability

--- logger_config.py
+++ logger_config.py
@@ -2,9 +2,9 @@ import logging
 
 
 class CustomFormatter(logging.Formatter):
-    COLOR_CODES = {
+    COLORS = {
         logging.DEBUG: "\x1b[38;20m",    # Grey
-        logging.INFO: "\x1b[38;20m",     # Grey (same as debug for consistency)
+        logging.INFO: "\x1b[38;20m",     # Grey
         logging.WARNING: "\x1b[33;20m",  # Yellow
         logging.ERROR: "\x1b[31;20m",    # Red
         logging.CRITICAL: "\x1b[31;1m",  # Bold Red
@@ -12,27 +12,26 @@ class CustomFormatter(logging.Formatter):
     RESET = "\x1b[0m"
     YELLOW = "\x1b[33;20m"
     GREEN = "\x1b[33;92m"
-    FORMAT = "%(message)s"
 
     def format(self, record):
-        color = self.COLOR_CODES.get(record.levelno, self.RESET)
-        log_fmt = f"{color}{self.FORMAT}{self.RESET}"
-        return logging.Formatter(log_fmt).format(record)
+        color = self.COLORS.get(record.levelno, self.RESET)
+        record.msg = f"{color}{record.msg}{self.RESET}"
+        return super().format(record)
 
     @staticmethod
     def configure_logger(name: str, level=logging.INFO):
         logger = logging.getLogger(name)
+        
+        # If logger already has handlers
+        if logger.hasHandlers():
+            return logger
 
-        if not logger.hasHandlers():  # Avoid adding handlers multiple times
-            logger.setLevel(level)
-
-        # Create a console handler with custom formatter
-        console_handler = logging.StreamHandler()
-        console_handler.setLevel(level)
-        console_handler.setFormatter(CustomFormatter())
-
-        # Add the handler to the logger and disable propagation
-        logger.addHandler(console_handler)
+        logger.setLevel(level)
         logger.propagate = False
+
+        # Create handler and assign the formatter
+        handler = logging.StreamHandler()
+        handler.setFormatter(CustomFormatter("%(message)s"))
+        logger.addHandler(handler)
 
         return logger
